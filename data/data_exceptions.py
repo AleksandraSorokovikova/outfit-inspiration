5777d37cc6878b00412666a00b6455558f5597ba
Женечек Ву <53404220+J3ne4ek@users.noreply.github.com>
2023-10-26 01:06:12 +0200

    Check runs (#5)
    fix types
    
    ---------
    
    Co-authored-by: evgeniia.vu <evgeniia.vu@jetbrains.com>

--- data/data_exceptions.py
+++ data/data_exceptions.py
@@ -1,33 +1,33 @@
 class ConnectionException(Exception):
-    def __init__(self, message):
+    def __init__(self, message: str):
         super().__init__(message)
 
 
 class UploadingException(Exception):
-    def __init__(self, message):
+    def __init__(self, message: str):
         super().__init__(message)
 
 
 class BucketSizeException(Exception):
-    def __init__(self, message):
+    def __init__(self, message: str):
         super().__init__(message)
 
 
 class BucketOverflowException(Exception):
-    def __init__(self, message):
+    def __init__(self, message: str):
         super().__init__(message)
 
 
 class FilesKeysExceptions(Exception):
-    def __init__(self, message):
+    def __init__(self, message: str):
         super().__init__(message)
 
 
 class GetFileExceptions(Exception):
-    def __init__(self, message):
+    def __init__(self, message: str):
         super().__init__(message)
 
 
 class GetSaveFileExceptions(Exception):
-    def __init__(self, message):
+    def __init__(self, message: str):
         super().__init__(message)
